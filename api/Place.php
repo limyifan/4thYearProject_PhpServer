e2af5cf3610dab9e6467b2da6a67a67e532bd82f
charles <as>
2020-01-24 12:54:59 +0000

    copy cormac changes

---
+++ api/Place.php
@@ -0,0 +1,51 @@
+<?php
+
+class Place{
+   
+    public $place_id;
+    public $place_name;
+    public $place_type;
+    public $rating;
+    public $latitude;
+    public $longitude;
+    public $icon;
+    public $open;
+    public $cover_image;
+
+
+    public function __construct($place_id, $place_name,$place_type,$rating, $latitude, $longitude, $icon, $open, $cover_image){
+        $this->place_id = $place_id;
+        $this->place_name = $place_name;
+        $this->place_type = $place_type;
+        $this->rating = $rating;
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+        $this->icon = $icon;
+        $this->open = $open;
+        //$this->price_level = $price_level;
+        $this->cover_image = $cover_image;
+    }
+    
+    function getName(){
+        return $this->place_name;
+    }
+
+    public function printPlace(){
+        echo "<br>" . $this->place_name . "\t" . $this->rating  . "\t <a>" . $this->cover_image . "</a>";
+    }
+    
+    public function setCoverImage(&$photo_reference){
+        $this->cover_image = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=". $photo_reference ."&key=" . $_SESSION["PLACES_API_KEY"];
+    }
+
+
+
+}
+
+
+
+
+
+
+
+?>
