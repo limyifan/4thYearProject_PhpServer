322fd8a472019fcf6e6a91d62c93239c938a8e7d
Cormac <cormacartcallan@hotmail.com>
2020-02-17 21:29:58 +0000

    pushing all files for release one

---
+++ Credentials.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//Google Places API
+$_SESSION["PLACES_API_KEY"]= "AIzaSyC-Qr_9Y10nFQMNzNtmOnuBf6QY3AuFCiw";
+
+
+
+
+?>
