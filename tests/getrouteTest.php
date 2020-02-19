b7fafa15d478230b594c4def5854465ba5b5fad3
charles <as>
2020-02-19 14:57:11 +0000

    fixed code for test to run successfully

--- tests/getrouteTest.php
+++ tests/getrouteTest.php
@@ -6,7 +6,7 @@ class PlaceTest extends PHPUnit_Framework_TestCase {
 
     public function testPlaceName() {
         //require 'Place.php';
-        $place = new Place(1, "name","musuem", 3, 23, 25, "", "open", "price_level");
+        $place = new Place(1, "name","musuem", 3, 23, 25, "", "open", "cover_image");
         $place->latitude = 23;
        $this->assertSame($place->getName(),"name");
     }
