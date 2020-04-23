8ef48e82031332c0ef9c777709c8198f4a1b61d4
Cormac <cormacartcallan@hotmail.com>
2020-04-23 21:17:30 +0100

    Added Postman page for simulating HTTP requests when testing V3

---
+++ api/v3/postman.php
@@ -0,0 +1,20 @@
+<?php
+
+
+//<a href="GetRoute.php?lat=&lng=&pref1=&pref2=History&pref3=Water">TEST API</a>
+?>
+
+<html>
+    
+    <a href="GetRoute.php?lat=&lng=&pref1=&pref2=&pref3=">0 Pref</a><br>
+    <a href="GetRoute.php?lat=&lng=&pref1=&pref2=&pref3=Food">1 Pref</a><br>
+    <a href="GetRoute.php?lat=&lng=&pref1=History&pref2=Entertainment&pref3=">2 Pref</a><br>
+    <a href="GetRoute.php?lat=&lng=&pref1=Sport&pref2=Arts_Culture&pref3=History">3 Pref</a><br>
+    
+    <br>
+    
+    
+    <a href="GetRoute.php?lat=&lng=&pref1=Entertainment=&pref2=History&pref3=Sport">Perfect</a>
+    
+    
+</html>
